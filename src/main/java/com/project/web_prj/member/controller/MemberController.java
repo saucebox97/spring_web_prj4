102bcacddb988440ee1fe7252e322823369f2114
saucebox97 <tjdqls970208@naver.com>
2022-08-05 15:17:37 +0900

    11시 11분

--- src/main/java/com/project/web_prj/member/controller/MemberController.java
+++ src/main/java/com/project/web_prj/member/controller/MemberController.java
@@ -1,7 +1,9 @@
 package com.project.web_prj.member.controller;
 
 import com.project.web_prj.member.domain.Member;
+import com.project.web_prj.member.domain.SNSLogin;
 import com.project.web_prj.member.dto.LoginDTO;
+import com.project.web_prj.member.service.KakaoService;
 import com.project.web_prj.member.service.LoginFlag;
 import com.project.web_prj.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import static com.project.web_prj.member.domain.OAuthValue.KAKAO_APP_KEY;
+import static com.project.web_prj.member.domain.OAuthValue.KAKAO_REDIRECT_URI;
 import static com.project.web_prj.util.LoginUtils.*;
 
 @Controller
@@ -26,6 +30,7 @@ import static com.project.web_prj.util.LoginUtils.*;
 public class MemberController {
 
     private final MemberService memberService;
+    private final KakaoService kakaoService;
 
     // 회원가입 양식 띄우기 요청
     @GetMapping("/sign-up")
@@ -55,7 +60,7 @@ public class MemberController {
 
     // 로그인 화면을 열어주는 요청처리
     @GetMapping("/sign-in") // url이랑 파일경로가똑같아서 void로해도됌 /boardinterceptor에서 로그인안됏을떄 message에 no-login이 담김
-    public void signIn(@ModelAttribute("message") String message, HttpServletRequest request) {
+    public void signIn(@ModelAttribute("message") String message, HttpServletRequest request, Model model) {
         log.info("/member/sign-in GET! - forwarding to sign-in.jsp");
 
         // 요청 정보 헤더 안에는 Referer라는 키가 있는데
@@ -64,6 +69,9 @@ public class MemberController {
         log.info("referer: {}", referer);
 
         request.getSession().setAttribute("redirectURI", referer);
+
+        model.addAttribute("kakaoAppKey", KAKAO_APP_KEY);
+        model.addAttribute("kakaoRedirect", KAKAO_REDIRECT_URI);
     }
 
     // 로그인 요청 처리 sign-in 29줄에서 아디/비번/자동로그인이옴
@@ -91,7 +99,7 @@ public class MemberController {
     }
 
     @GetMapping("/sign-out")
-    public String signOut(HttpServletRequest request, HttpServletResponse response) {
+    public String signOut(HttpServletRequest request, HttpServletResponse response) throws Exception{
 
         HttpSession session = request.getSession();
 
@@ -102,8 +110,23 @@ public class MemberController {
                 memberService.autoLogout(getCurrentMemberAccount(session), request, response);
             }
 
+            // SNS 로그인 상태라면 해당 SNS 로그아웃 처리를 진행한다
+            SNSLogin from = (SNSLogin) session.getAttribute(LOGIN_FROM);
+            switch (from) {
+                case KAKAO:
+                    kakaoService.logout((String) session.getAttribute("accessToken"));
+                    break;
+                case NAVER:
+                    break;
+                case GOOGLE:
+                    break;
+                case FACEBOOK:
+                    break;
+            }
+
             // 1. 세션에서 정보를 삭제한다.
             session.removeAttribute(LOGIN_FLAG);
+
             // 2. 세션을 무료화한다.
             session.invalidate();
             return "redirect:/";
---
+++ src/main/java/com/project/web_prj/member/domain/OAuthValue.java
@@ -0,0 +1,7 @@
+package com.project.web_prj.member.domain;
+
+public interface OAuthValue {
+
+    String KAKAO_APP_KEY = "c4314a184d9f64ffcddbaa7db7367496"; // 앱키 -> REST API키
+    String KAKAO_REDIRECT_URI = "/oauth/kakao";
+}
